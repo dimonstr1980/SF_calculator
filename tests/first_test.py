ada1b0d9e696b67c10a0641b46eb6b0a3e5631fd
Дмитрий Дьячков <108222110+dimonstr1980@users.noreply.github.com>
2022-12-01 23:54:28 +0300

    Add some examples

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,5 +1,6 @@
 import pytest
 import math
+import task
 from app.calculator import Calculator
 
 
@@ -54,42 +55,44 @@ class TestCalc:
 
     def test_factorial_calculate_failed(self):
         assert self.calc.factorial(self, 5) == 1
-    # Пример 1
-    def task_1(a, d, c):
+        
+    def test_1(self):
+        a, d, c = 3, 5, 7
         ans = (c - d / 5 + math.sqrt(321)) / (1 + a * 3)
-        return ans
+        self.assertEqual(task.task_1(3, 5, 7), ans)
 
-    # Пример 2
-    def task_2(a, d, c):
-        ans = (math.log10(c / 3) - d + 25) / (a * 5 - 1)
-        return ans
+    def test_2(self):
+        a,d,c = 2,4,6
+        ans = (math.log10(c/3)-d+25)/(a*5-1)
+        self.assertEqual(task.task_2(2, 4, 6), ans)
 
-    # Пример 3
-    def task_3(a, d, c):
-        ans = (c / 2 + math.log(d) - 35) / (a * 5 + 1)
-        return ans
+    def test_3(self):
+        a,d,c = 9,8,7
+        ans = (c/2+math.log(d)-35)/(a*5+1)
+        self.assertEqual(task.task_3(9, 8, 7), ans)
 
-    # Пример 4
-    def task_4(a, d, c):
-        ans = (math.tan(c) + d / 32) / (a / 3 + 1)
-        return ans
+    def test_4(self):
+        a,d,c = 3,4,5
+        ans = (math.tan(c)+d/32)/(a/3+1)
+        self.assertEqual(task.task_4(3, 4, 5), ans)
 
-    # Пример 5
-    def task_5(a, d, c):
-        ans = (c / 5 - d + 1) / (c + math.tan(2 * a))
-        return ans
+    def test_5(self):
+        a,d,c = 2,3,4
+        ans = (c/5-d+1)/(c+math.tan(2*a))
+        self.assertEqual(task.task_5(2, 3, 4), ans)
 
-    # Пример 6
-    def task_6(a, d, c):
-        ans = (math.sqrt(25 * c) + d - 3) / (d - a * a + 1)
-        return ans
+    def test_6(self):
+        a,d,c = 5,6,3
+        ans = (math.sqrt(25*c)+d-3)/(d-a*a+1)
+        self.assertEqual(task.task_6(5, 6, 3), ans)
 
-    # Пример 7
-    def task_7(a, d, c):
-        ans = (5 * math.log10(c) + 3 * d - 32) / (a * a + 1)
-        return ans
+    def test_7(self):
+        a,d,c = 6,7,8
+        ans = (5*math.log10(c)+3*d-32)/(a*a+1)
+        self.assertEqual(task.task_7(6, 7, 8), ans)
 
-    # Пример 8
-    def task_8(a, d, c):
-        ans = (c * d - math.log(4 * 3 * a)) / (c + a - 1)
-        return ans
+    def test_8(self):
+        a,d,c = 2,9,5
+        ans = (c*d-math.log(4*3*a))/(c+a-1)
+        self.assertEqual(task.task_8(2, 9, 5), ans)
+        
