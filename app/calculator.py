be409a1a82f61818fced96e14d433f878efa0dd3
Дмитрий Дьячков <ddimonstr@yandex.ru>
2022-12-20 06:07:38 +0300

    Update files

--- app/calculator.py
+++ app/calculator.py
@@ -1,4 +1,4 @@
-import math
+from math import *
 
 
 class Calculator:
@@ -28,36 +28,37 @@ class Calculator:
         if x == 1:
             return 1
         else:
-            return x * math.factorial(x - 1)
+            return x * factorial(x - 1)
     
-    def task_1(a, d, c):
-        ans = (c - d / 5 + math.sqrt(321)) / (1 + a * 3)
+    def task_1(self, a, d, c):
+        ans = (c - d / 5 + sqrt(321)) / (1 + a * 3)
         return ans
 
-    def task_2(a, d, c):
-        ans = (math.log10(c / 3) - d + 25) / (a * 5 - 1)
+    def task_2(self, a, d, c):
+        ans = (log10(c / 3) - d + 25) / (a * 5 - 1)
         return ans
 
-    def task_3(a, d, c):
-        ans = (c / 2 + math.log(d) - 35) / (a * 5 + 1)
+    def task_3(self, a, d, c):
+        ans = (c / 2 + log(d) - 35) / (a * 5 + 1)
         return ans
 
-    def task_4(a, d, c):
-        ans = (math.tan(c) + d / 32) / (a / 3 + 1)
+    def task_4(self, a, d, c):
+        ans = (tan(c) + d / 32) / (a / 3 + 1)
         return ans
 
-    def task_5(a, d, c):
-        ans = (c / 5 - d + 1) / (c + math.tan(2 * a))
+    def task_5(self, a, d, c):
+        ans = (c / 5 - d + 1) / (c + tan(2 * a))
         return ans
 
-    def task_6(a, d, c):
-        ans = (math.sqrt(25 * c) + d - 3) / (d - a * a + 1)
+    def task_6(self, a, d, c):
+        ans = (sqrt(25 * c) + d - 3) / (d - a * a + 1)
         return ans
 
-    def task_7(a, d, c):
-        ans = (5 * math.log10(c) + 3 * d - 32) / (a * a + 1)
+    def task_7(self, a, d, c):
+        ans = (5 * log10(c) + 3 * d - 32) / (a * a + 1)
         return ans
 
-    def task_8(a, d, c):
-        ans = (c * d - math.log(4 * 3 * a)) / (c + a - 1)
+    def task_8(self, a, d, c):
+        ans = (c * d - log(4 * 3 * a)) / (c + a - 1)
         return ans
+        
--- tests/first_test.py
+++ tests/first_test.py
@@ -1,6 +1,4 @@
-import pytest
-import math
-import task
+from math import *
 from app.calculator import Calculator
 
 
@@ -12,87 +10,88 @@ class TestCalc:
         assert self.calc.multiply(self, 2, 2) == 4
 
     def test_multiply_calculate_failed(self):
-        assert self.calc.multiply(self, 2, 2) == 1
+        assert self.calc.multiply(self, 2, 2) != 4
 
     def test_division_calculate_correctly(self):
         assert self.calc.division(self, 2, 2) == 1
 
     def test_division_calculate_failed(self):
-        assert self.calc.multiply(self, 2, 2) == 0
+        assert self.calc.division(self, 2, 2) != 1
 
     def test_subtraction_calculate_correctly(self):
         assert self.calc.subtraction(self, 2, 2) == 0
 
     def test_subtraction_calculate_failed(self):
-        assert self.calc.subtraction(self, 2, 2) == 1
+        assert self.calc.subtraction(self, 2, 2) != 0
 
     def test_adding_calculate_correctly(self):
         assert self.calc.adding(self, 2, 2) == 4
 
     def test_adding_calculate_failed(self):
-        assert self.calc.adding(self, 2, 2) == 1
+        assert self.calc.adding(self, 2, 2) != 4
 
     def test_modulus_calculate_correctly(self):
         assert self.calc.modulus(self, 2, 2) == 0
 
     def test_modulus_calculate_failed(self):
-        assert self.calc.modulus(self, 2, 2) == 1
+        assert self.calc.modulus(self, 2, 2) != 0
 
     def test_square_calculate_correctly(self):
         assert self.calc.square(self, 2) == 4
 
     def test_square_calculate_failed(self):
-        assert self.calc.square(self, 2) == 1
+        assert self.calc.square(self, 2) != 4
 
     def test_square_root_calculate_correctly(self):
         assert self.calc.square_root(self, 9) == 3
 
     def test_square_root_calculate_failed(self):
-        assert self.calc.square_root(self, 9) == 1
+        assert self.calc.square_root(self, 9) != 3
 
     def test_factorial_calculate_correctly(self):
         assert self.calc.factorial(self, 5) == 120
 
     def test_factorial_calculate_failed(self):
-        assert self.calc.factorial(self, 5) == 1
+        assert self.calc.factorial(self, 5) != 120
         
     def test_1(self):
         a, d, c = 3, 5, 7
-        ans = (c - d / 5 + math.sqrt(321)) / (1 + a * 3)
-        self.assertEqual(task.task_1(3, 5, 7), ans)
+        ans = (c - d / 5 + sqrt(321)) / (1 + a * 3)
+        assert self.calc.task_1(self, 3, 5, 7) == ans
 
     def test_2(self):
-        a,d,c = 2,4,6
-        ans = (math.log10(c/3)-d+25)/(a*5-1)
-        self.assertEqual(task.task_2(2, 4, 6), ans)
+        a, d, c = 2, 4, 6
+        ans = (log10(c / 3) - d + 25) / (a * 5 - 1)
+        assert self.calc.task_2(self, 2, 4, 6) == ans
 
     def test_3(self):
-        a,d,c = 9,8,7
-        ans = (c/2+math.log(d)-35)/(a*5+1)
-        self.assertEqual(task.task_3(9, 8, 7), ans)
+        a, d, c = 9, 8, 7
+        ans = (c / 2 + log(d) - 35) / (a * 5 + 1)
+        assert self.calc.task_3(self, 9, 8, 7) == ans
 
     def test_4(self):
-        a,d,c = 3,4,5
-        ans = (math.tan(c)+d/32)/(a/3+1)
-        self.assertEqual(task.task_4(3, 4, 5), ans)
+        a, d, c = 3, 4, 5
+        ans = (tan(c) + d / 32) / (a / 3 + 1)
+        assert self.calc.task_4(self, 3, 4, 5) == ans
 
     def test_5(self):
-        a,d,c = 2,3,4
-        ans = (c/5-d+1)/(c+math.tan(2*a))
-        self.assertEqual(task.task_5(2, 3, 4), ans)
+        a, d, c = 2, 3, 4
+        ans = (c / 5 - d + 1) / (c + tan(2 * a))
+        assert self.calc.task_5(self, 2, 3, 4) == ans
 
     def test_6(self):
-        a,d,c = 5,6,3
-        ans = (math.sqrt(25*c)+d-3)/(d-a*a+1)
-        self.assertEqual(task.task_6(5, 6, 3), ans)
+        a, d, c = 5, 6, 3
+        ans = (sqrt(25 * c) + d - 3) / (d - a * a + 1)
+        assert self.calc.task_6(self, 5, 6, 3) == ans
 
     def test_7(self):
-        a,d,c = 6,7,8
-        ans = (5*math.log10(c)+3*d-32)/(a*a+1)
-        self.assertEqual(task.task_7(6, 7, 8), ans)
+        a, d, c = 6, 7, 8
+        ans = (5 * log10(c) + 3 * d - 32) / ( a * a + 1)
+        assert self.calc.task_7(self, 6, 7, 8) == ans
 
     def test_8(self):
-        a,d,c = 2,9,5
-        ans = (c*d-math.log(4*3*a))/(c+a-1)
-        self.assertEqual(task.task_8(2, 9, 5), ans)
+        a, d, c = 2, 9, 5
+        ans = (c * d - log(4 * 3 * a)) / (c + a - 1)
+        assert self.calc.task_8(self, 2, 9, 5) == ans
+
         
